a89fdebafc08e36222b7e45e79e9cc042c37b9f5
Hitesh <hitesh@localhost.localdomain>
2023-07-13 00:48:34 +0530

    admin and user auth added

--- dist/controller/user.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userLoginController = void 0;
-const user_service_1 = require("../services/user.service");
-const userLoginController = (req, res, next) => (0, user_service_1.userLoginService)(req, res, next);
-exports.userLoginController = userLoginController;
-//# sourceMappingURL=user.controller.js.map
--- dist/services/user.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userLoginService = void 0;
-const userLoginService = (req, res, next) => {
-    return res.status(200).json({ message: 'nice' });
-};
-exports.userLoginService = userLoginService;
-//# sourceMappingURL=user.service.js.map
